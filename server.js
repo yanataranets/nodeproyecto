84eb7556c987103cc927434ca88f7081b7bc9a4d
yanataranets <yana.taranet.s@gmail.com>
2021-02-15 11:53:48 +0100

    filezilla connection

--- server.js
+++ server.js
@@ -45,32 +45,119 @@ new Ignitor(require('@adonisjs/fold'))
 // )
 //
 // app.listen(3000, () => console.log('listening port 3000..'))
-const Client = require('ssh2').Client;
-const connSettings = {
-  host: 'access828337811.webspace-data.io',
-  port: 22,
-  username: 'u100957214',
-  password: 'TAFNadmin.001'
-};
-const remotePathToList = './clickandbuilds/Joomla/Audinetwork/Userdocument';
-const conn = new Client();
-conn.on('ready', function() {
-  conn.sftp(function(err, sftp) {
-    if (err) throw err;
-    sftp.readdir(remotePathToList, function (err, list) {
-      if (err) throw err;
-      const fs = require("fs"); // Use node filesystem
-      const readStream = fs.createReadStream("tmp/uploads/1613032382187.pdf");
-      const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument/1613032382187.pdf");
-      writeStream.on('close', function () {
-        console.log("done");
-      });
-      writeStream.on('end', function () {
-        console.log("connection closed");
-        conn.close();
-      });
-//go
-      readStream.pipe(writeStream);
-    });
-  });
-  }).connect(connSettings);
+// const Client = require('ssh2').Client;
+// const connSettings = {
+//   host: 'access828337811.webspace-data.io',
+//   port: 22,
+//   username: 'u100957214',
+//   password: 'TAFNadmin.001'
+// };
+// const remotePathToList = './clickandbuilds/Joomla/Audinetwork/Userdocument';
+// const conn = new Client();
+// conn.on('ready', function() {
+//   conn.sftp(function(err, sftp) {
+//     if (err) throw err;
+//     sftp.readdir(remotePathToList, function (err, list) {
+//       if (err) throw err;
+//       const fs = require("fs"); // Use node filesystem
+//       // const fileUrl = new URL('tmp/uploads/');
+//       // const fileToUrl new URL ()
+//
+//       // var moveFrom = "tmp/uploads/";
+//       // var moveTo = "./clickandbuilds/Joomla/Audinetwork/Userdocument/1613032382187.pdf";
+//       //
+//       // sftp.fastGet(moveFrom, moveTo, {}, function(downloadError){
+//       //   if(downloadError) throw downloadError;
+//       //
+//       //   console.log("done");
+//       // })
+//       // const readStream = fs.readFile(
+//       //   'tmp/uploads/'
+//       // )
+//       // const writeStream = fs.writeFile(
+//       //   './clickandbuilds/Joomla/Audinetwork/Userdocument/',
+//       // )
+//       // correcto
+//       //const readStream = fs.createReadStream("tmp/uploads/1613032382187.pdf");
+//       const readStream = fs.createReadStream("tmp/uploads/1613031902075.pdf");
+//       const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument/1613031902075.pdf");
+//       writeStream.on('close', function () {
+//         console.log("done");
+//       });
+//       writeStream.on('end', function () {
+//         console.log("connection closed");
+//         conn.close();
+//       });
+// //go
+//       readStream.pipe(writeStream);
+//
+//       // const options = Object.assign({}, {
+//       //   encoding: 'utf-8'
+//       // }, true)
+//       // const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument");
+//       // const data = writeStream.end(fileStreamContent);
+//       // writeStream.on('close', function(){
+//       //   console.log("done");
+//       //   conn.end();
+//       // });
+//     });
+//   });
+//   }).connect(connSettings);
+// const Client = require('ssh2').Client;
+// const connSettings = {
+//   host: 'access828337811.webspace-data.io',
+//   port: 22,
+//   username: 'u100957214',
+//   password: 'TAFNadmin.001'
+// };
+// const remotePathToList = './clickandbuilds/Joomla/Audinetwork/Userdocument';
+// const conn = new Client();
+// conn.on('ready', function() {
+//   conn.sftp(function(err, sftp) {
+//     if (err) throw err;
+//     sftp.readdir(remotePathToList, function (err, list) {
+//       if (err) throw err;
+//       const fs = require("fs"); // Use node filesystem
+//       // const fileUrl = new URL('tmp/uploads/');
+//       // const fileToUrl new URL ()
+//
+//       // var moveFrom = "tmp/uploads/";
+//       // var moveTo = "./clickandbuilds/Joomla/Audinetwork/Userdocument/1613032382187.pdf";
+//       //
+//       // sftp.fastGet(moveFrom, moveTo, {}, function(downloadError){
+//       //   if(downloadError) throw downloadError;
+//       //
+//       //   console.log("done");
+//       // })
+//       // const readStream = fs.readFile(
+//       //   'tmp/uploads/'
+//       // )
+//       // const writeStream = fs.writeFile(
+//       //   './clickandbuilds/Joomla/Audinetwork/Userdocument/',
+//       // )
+//       // correcto
+//       //const readStream = fs.createReadStream("tmp/uploads/1613032382187.pdf");
+//       const readStream = fs.createReadStream("tmp/uploads/1613031902075.pdf");
+//       const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument/1613031902075.pdf");
+//       writeStream.on('close', function () {
+//         console.log("done");
+//       });
+//       writeStream.on('end', function () {
+//         console.log("connection closed");
+//         conn.close();
+//       });
+// //go
+//       readStream.pipe(writeStream);
+//
+//       // const options = Object.assign({}, {
+//       //   encoding: 'utf-8'
+//       // }, true)
+//       // const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument");
+//       // const data = writeStream.end(fileStreamContent);
+//       // writeStream.on('close', function(){
+//       //   console.log("done");
+//       //   conn.end();
+//       // });
+//     });
+//   });
+//   }).connect(connSettings);
--- start/routes.js
+++ start/routes.js
@@ -38,7 +38,19 @@ Route.post('password/reset', 'Auth/PasswordResetController.reset')
 
 
 //
+const Client = require('ssh2').Client;
+const connSettings = {
+  host: 'access828337811.webspace-data.io',
+  port: 22,
+  username: 'u100957214',
+  password: 'TAFNadmin.001'
+};
+const remotePathToList = './clickandbuilds/Joomla/Audinetwork/Userdocument';
+const conn = new Client();
+
 const Helpers = use('Helpers')
+// const rutaVari = null;
+let filename = '';
 
 Route.post('/upload', async ({ request }) => {
   const profileFiles = request.file('profile_file', {
@@ -46,15 +58,117 @@ Route.post('/upload', async ({ request }) => {
     size: '20mb',
     extname: ['jpeg', 'png', 'jpg']
   })
-
+  filename = Date.now()+'.pdf';
   await profileFiles.move(Helpers.tmpPath('uploads'), {
-    name: Date.now()+'.pdf',
+    name: filename,
     overwrite: false
   })
-
   if (!profileFiles.moved()) {
     return profileFiles.error()
   }
+
+  conn.on('ready', function() {
+    conn.sftp(function(err, sftp) {
+      if (err) throw err;
+      sftp.readdir(remotePathToList, function (err, list) {
+        if (err) throw err;
+        const fs = require("fs");
+        // Use node filesystem
+        // const fileUrl = new URL('tmp/uploads/');
+        // const fileToUrl new URL ()
+        // var moveFrom = "tmp/uploads/";
+        // var moveTo = "./clickandbuilds/Joomla/Audinetwork/Userdocument/1613032382187.pdf";
+        //
+        // sftp.fastGet(moveFrom, moveTo, {}, function(downloadError){
+        //   if(downloadError) throw downloadError;
+        //
+        //   console.log("done");
+        // })
+        // const readStream = fs.readFile(
+        //   'tmp/uploads/'
+        // )
+        // const writeStream = fs.writeFile(
+        //   './clickandbuilds/Joomla/Audinetwork/Userdocument/',
+        // )
+        // correcto
+        //const readStream = fs.createReadStream("tmp/uploads/1613032382187.pdf");
+        //const readStream = fs.createReadStream("C:/Users/Admin/Desktop/adonis-main/tmp/uploads/1613032382187.pdf")
+        const readStream = fs.createReadStream(Helpers.tmpPath('uploads')+'/'+filename);
+        const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument/"+filename);
+        writeStream.on('close', function () {
+          console.log("done");
+        });
+        writeStream.on('end', function () {
+          console.log("connection closed");
+          conn.close();
+        });
+//go
+        readStream.pipe(writeStream);
+
+        // const options = Object.assign({}, {
+        //   encoding: 'utf-8'
+        // }, true)
+        // const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument");
+        // const data = writeStream.end(fileStreamContent);
+        // writeStream.on('close', function(){
+        //   console.log("done");
+        //   conn.end();
+        // });
+      });
+    });
+  }).connect(connSettings);
+
   return ('done')
 })
 
+//
+// conn.on('ready', function() {
+//   conn.sftp(function(err, sftp) {
+//     if (err) throw err;
+//     sftp.readdir(remotePathToList, function (err, list) {
+//       if (err) throw err;
+//       const fs = require("fs");
+//       // Use node filesystem
+//       // const fileUrl = new URL('tmp/uploads/');
+//       // const fileToUrl new URL ()
+//       // var moveFrom = "tmp/uploads/";
+//       // var moveTo = "./clickandbuilds/Joomla/Audinetwork/Userdocument/1613032382187.pdf";
+//       //
+//       // sftp.fastGet(moveFrom, moveTo, {}, function(downloadError){
+//       //   if(downloadError) throw downloadError;
+//       //
+//       //   console.log("done");
+//       // })
+//       // const readStream = fs.readFile(
+//       //   'tmp/uploads/'
+//       // )
+//       // const writeStream = fs.writeFile(
+//       //   './clickandbuilds/Joomla/Audinetwork/Userdocument/',
+//       // )
+//       // correcto
+//       //const readStream = fs.createReadStream("tmp/uploads/1613032382187.pdf");
+//       //const readStream = fs.createReadStream("C:/Users/Admin/Desktop/adonis-main/tmp/uploads/1613032382187.pdf")
+//       const readStream = fs.createReadStream(Helpers.tmpPath('uploads')+'/'+filename);
+//       const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument/"+filename);
+//       writeStream.on('close', function () {
+//         console.log("done");
+//       });
+//       writeStream.on('end', function () {
+//         console.log("connection closed");
+//         conn.close();
+//       });
+// //go
+//       readStream.pipe(writeStream);
+//
+//       // const options = Object.assign({}, {
+//       //   encoding: 'utf-8'
+//       // }, true)
+//       // const writeStream = sftp.createWriteStream("./clickandbuilds/Joomla/Audinetwork/Userdocument");
+//       // const data = writeStream.end(fileStreamContent);
+//       // writeStream.on('close', function(){
+//       //   console.log("done");
+//       //   conn.end();
+//       // });
+//     });
+//   });
+// }).connect(connSettings);
